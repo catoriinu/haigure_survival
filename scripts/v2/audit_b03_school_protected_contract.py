fb2caf5305f3b50960a6bf19a79e2c13a567bdd3
catoriinu <catoriinu8190@gmail.com>
2026-07-20 21:05:56 +0900

    fix: 学校1階閉鎖階段の踊り場開口を復元

--- scripts/v2/audit_b03_school_protected_contract.py
+++ scripts/v2/audit_b03_school_protected_contract.py
@@ -34,7 +34,7 @@ PROTECTED_PREFIXES = (
 ALLOWED_NEW_PREFIXES = ("COL_B03_Interior_",)
 ALLOWED_NEW_EXACT_NAMES = {"NAV_Blocker_Interiors"}
 T04_CORRECTION_VERSION_PROPERTY = "t04_2b_nav_connectivity_version"
-T04_CORRECTION_VERSION = "t04-2b-nav-connectivity-v01"
+T04_CORRECTION_VERSION = "t04-2b-nav-connectivity-v02"
 T04_ALLOWED_CHANGED_EXACT_NAMES = {
     "COL_B03_Ceiling_F02",
     "COL_B03_Ceiling_F03",
