c2bcdcce50b730f8f723d2fcdc1c06b9942e45ab
catoriinu <catoriinu8190@gmail.com>
2026-07-20 21:16:17 +0900

    fix: 学校上階NavMeshへ衝突障害物を反映

--- scripts/v2/audit_b03_school_protected_contract.py
+++ scripts/v2/audit_b03_school_protected_contract.py
@@ -34,7 +34,7 @@ PROTECTED_PREFIXES = (
 ALLOWED_NEW_PREFIXES = ("COL_B03_Interior_",)
 ALLOWED_NEW_EXACT_NAMES = {"NAV_Blocker_Interiors"}
 T04_CORRECTION_VERSION_PROPERTY = "t04_2b_nav_connectivity_version"
-T04_CORRECTION_VERSION = "t04-2b-nav-connectivity-v02"
+T04_CORRECTION_VERSION = "t04-2b-nav-connectivity-v03"
 T04_ALLOWED_CHANGED_EXACT_NAMES = {
     "COL_B03_Ceiling_F02",
     "COL_B03_Ceiling_F03",
@@ -45,6 +45,7 @@ T04_ALLOWED_CHANGED_EXACT_NAMES = {
     "NAV_Walkable_Interior3F",
     "NAV_Walkable_Interior4F",
     "NAV_Walkable_StairsNWUpper",
+    "NAV_Blocker_SchoolUpper",
 }
 
 
